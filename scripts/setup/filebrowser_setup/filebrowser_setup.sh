9f8db6f30adacc88d6317f14d8da579726f75e31
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2025-06-21 19:46:28 +0200

    mmm

--- scripts/setup/filebrowser_setup/filebrowser_setup.sh
+++ scripts/setup/filebrowser_setup/filebrowser_setup.sh
@@ -10,35 +10,31 @@ dnf update -y
 prompt() {
   local prompt_message=$1
   local default_value=$2
-  read -r -p "$prompt_message [$default_value]: " input
+  read -p "$prompt_message [$default_value]: " input
   echo "${input:-$default_value}"
 }
 
-storage_path=$(prompt "Enter the storage path for FileBrowser" "/storage/filebrowser")
-port=$(prompt "Enter the port number for FileBrowser" "8086")
+filebrowser_volume=$(prompt "Enter the volume to be browsed" "/storage")
+filebrowser_username=$(prompt "Enter your username" "admin")
+filebrowser_password=$(prompt "Enter your password" "changeme")
+filebrowser_port=$(prompt "Enter the port number" "8086")
 
-# Create directories and empty config files to ensure correct permissions and existence
-mkdir -p "$storage_path/srv"
-touch "$storage_path/filebrowser.db"
-touch "$storage_path/filebrowser.json"
 
 cat <<EOF > docker-compose.yaml
 services:
   filebrowser:
-    image: filebrowser/filebrowser
-    container_name: filebrowser
-    user: "1000:1000"
-    ports:
-      - "$port:80"
+    image: filebrowser/filebrowser:latest
+    container_name: file-manager
     volumes:
-      - "$storage_path/srv:/srv"
-      - "$storage_path/filebrowser.db:/database.db"
-      - "$storage_path/filebrowser.json:/.filebrowser.json"
-    restart: always
+      - $filebrowser_volume:/srv
+    ports:
+      - "$filebrowser_port:80"
+    environment:
+      FB_BASEURL: "/"
+    restart: unless-stopped
 EOF
 
 echo "The docker-compose.yaml has been created successfully."
-echo "Default credentials are admin / admin. Please change them on first login."
 
 docker compose up -d
 docker ps
